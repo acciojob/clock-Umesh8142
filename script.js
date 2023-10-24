e2f063b6ecf6aecced0b3735c50d714c499b0e68
N UMESH <102941752+Umesh8142@users.noreply.github.com>
2023-10-24 16:17:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+function updateTimer() {
+    const timerElement = document.getElementById("timer");
+    const currentDate = new Date();
+
+    const options = {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+    };
+
+    const formattedDate = currentDate.toLocaleDateString('en-US', options);
+    timerElement.textContent = formattedDate;
+}
+
+// Update the timer every second
+setInterval(updateTimer, 1000);
+
+// Initial call to display the timer immediately
+updateTimer();
